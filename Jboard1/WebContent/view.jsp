2c4561ed1cb2372bd7140549f787e0a3efcab15b
tsbslteisrhy <tsbslteisrhy@naver.com>
2020-06-10 11:38:12 +0900

    2020/06/10 Jboard1 실습 추가

--- Jboard1/WebContent/view.jsp
+++ Jboard1/WebContent/view.jsp
@@ -12,6 +12,7 @@
 		
 	if(mb == null){		
 		response.sendRedirect("/Jboard1/user/login.jsp");
+		return;
 	}
 	
 	request.setCharacterEncoding("utf-8");	
@@ -107,6 +108,7 @@
             <!-- 댓글리스트 -->
             <section class="commentList">
                 <h3>댓글목록</h3>
+                <% if(article.getComment() > 0){ %>
                 <article class="comment">
                     <span>
                         <span>길동이</span>
@@ -118,13 +120,17 @@
                         <a href="#">수정</a>
                     </div>
                 </article>
+                <% } else { %>
                 <p class="empty">등록된 댓글이 없습니다.</p>
+                <% } %>
             </section>
 
             <!-- 댓글입력폼 -->
             <section class="commentForm">
                 <h3>댓글쓰기</h3>
-                <form action="#">
+                <form action="/Jboard1/proc/comment.jsp" method="post">
+                	<input type="hidden" name="parent" value="<%= article.getSeq() %>"/>
+                	<input type="hidden" name="uid" value="<%= mb.getUid() %>"/>
                     <textarea name="comment"></textarea>
                     <div>
                         <a href="#" class="btnCancel">취소</a>
--- Jboard1/src/kr/co/jboard1/config/SQL.java
+++ Jboard1/src/kr/co/jboard1/config/SQL.java
@@ -8,7 +8,11 @@ public class SQL {
 	
 	
 	// 게시물 관련
+	public final static String SELECT_TOTAL_COUNT ="SELECT COUNT(`seq`) FROM `JBOARD_ARTICLE` "
+													+ "WHERE `parent`=0";
+	
 	public final static String DELETE_ARTICLE = "DELETE FROM `JBOARD_ARTICLE` WHERE `seq`=?";
+	
 	public final static String UPDATE_HIT = "UPDATE `JBOARD_ARTICLE` SET `hit`=`hit`+1 WHERE `seq`=?";
 	
 	public final static String SELECT_ARTICLE = "SELECT * FROM `JBOARD_ARTICLE` "
@@ -16,7 +20,10 @@ public class SQL {
 	
 	public final static String SELECT_ARTICLES = "SELECT a.*, b.nick FROM `JBOARD_ARTICLE` AS a "  
 												+ "JOIN `JBOARD_MEMBER` AS b "
-												+ "ON a.uid = b.uid";
+												+ "ON a.uid = b.uid "
+												+ "WHERE `parent`=0 "
+												+ "ORDER BY `seq` DESC "
+												+ "LIMIT ?, 10";
 	
 	public final static String INSERT_ARTICLE = "INSERT INTO `JBOARD_ARTICLE` SET "
 												+ "`title`=?, "
@@ -25,6 +32,14 @@ public class SQL {
 												+ "`regip`=?, "
 												+ "`rdate`=NOW()";
 		   
+	public final static String INSERT_COMMENT = "INSERT INTO `JBOARD_ARTICLE` SET "
+												+ "`parent`=?, "
+												+ "`content`=?, "
+												+ "`uid`=?, "
+												+ "`regip`=?, "
+												+ "`rdate`=NOW()";
 	
+	public final static String UPDATE_COMMENT_COUNT = "UPDATE `JBOARD_ARTICLE` SET `comment` = `comment` + 1 "
+													+ "WHERE `seq`=?";
 	
 }
