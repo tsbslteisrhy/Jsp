a9152b274d3aae46f7eee5635524e1d1620047ad
bigdata1 <tsbslteisrhy@naver.com>
2020-07-14 12:41:40 +0900

    2020/07/14 실습추가

---
+++ Ch18/src/controller/MainController.java
@@ -0,0 +1,97 @@
+package controller;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.sun.javafx.image.impl.General;
+
+import model.CommonService;
+import model.GreetingService;
+import model.HelloService;
+import model.WelcomeService;
+
+public class MainController extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+	private Map<String, Object> instances = new HashMap<>();
+	
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+		// 프로퍼티 파일 경로 구하기
+		ServletContext sc = config.getServletContext();
+		String path = sc.getRealPath("/WEB-INF")+"/urlMapping.properties";
+		
+		Properties prop = new Properties();
+		
+		try {
+			// 프로퍼티 파일과 입력 스트림 연결
+			FileInputStream fis = new FileInputStream(path);
+			// 입력 스트림으로 프로퍼티 파일 읽어서 prop객체에 Key-Map 구조로 저장
+			prop.load(fis);
+			// 입력 스트림 해제
+			fis.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		// prop 객체를 가지고 동적 Model 객체를 Map에 저장
+		Iterator iter = prop.keySet().iterator();
+		
+		while(iter.hasNext()) {
+			
+			String k = iter.next().toString();
+			String v = prop.getProperty(k);
+			
+			try {
+				Class obj = Class.forName(v);
+				Object instance = obj.newInstance();
+				
+				instances.put(k, instance);
+				
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		requestProc(req, resp);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		requestProc(req, resp);
+	}
+	
+	private void requestProc(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String path = req.getContextPath();	// /ch18
+		String uri  = req.getRequestURI();	// /ch18/hello.do
+		
+		String action = uri.substring(path.length());	// /hello.do
+		
+		CommonService instance = (CommonService)instances.get(action);
+		String view = instance.requestProc(req, resp);
+
+		// View 포워드
+		RequestDispatcher dispatcher = req.getRequestDispatcher(view);
+		dispatcher.forward(req, resp);
+		
+	}
+	
+}
---
+++ Ch18/src/model/WelcomeService.java
@@ -0,0 +1,17 @@
+package model;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WelcomeService implements CommonService {
+
+	@Override
+	public String requestProc(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
+		
+		return "/welcome.jsp";
+	}
+
+}
